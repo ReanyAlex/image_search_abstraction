3bd593809f65874ee2d03fb3553c866acebd6a57
Alex Reany <reanyalex@gmail.com>
2017-07-16 14:38:29 -0700

    testing heroku

--- server.js
+++ server.js
@@ -17,22 +17,24 @@ const Search = require("./models/searchSchema")
 
 
 
-// if (!process.env.DISABLE_XORIGIN) {
-//   app.use(function(req, res, next) {
-//     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-//     var origin = req.headers.origin || '*';
-//     if (!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
-//       console.log(origin);
-//       res.setHeader('Access-Control-Allow-Origin', origin);
-//       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     }
-//     next();
-//   });
-// }
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if (!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+      console.log(origin);
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
 
-app.use('/public', express.static(process.cwd() + '/public'));
 const url = process.env.DATABASEURLTD || "mongodb://localhost/image_search_app"
 mongoose.connect(url);
+
+app.use(express.static("public"));
+// app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(imagesearchRoutes);
 app.use(historyRoutes);
